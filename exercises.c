956a76b9979a1760f1e9cc020f16ca9cf916b469
Replit user <>
2023-08-17 20:40:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,17 +39,28 @@ void reverseArray(int arr[], int size)
       aux = arr[j];
       arr[j] = arr[i];
       arr[i] = aux;
-      j--;
-        
+      j--;        
     }
   }
+
 /*
 Ejercicio 3: Filtrar Números Pares
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+int *filterEvenNumbers(int arr[], int size, int *newSize) 
+{ int newArr[*newSize];
+ int cont = 0
+  for (int i = 0; i < size; i++)
+    {
+      if (arr[i]%2 == 0)
+      {
+        newArr[cont] = arr[i];
+        
+      }
+    }
+  return NULL; }
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
