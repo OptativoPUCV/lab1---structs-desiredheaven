958b1bbfeeeb4e82fa8940e5205ff293f5349b61
Replit user <>
2023-08-19 00:59:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -99,13 +99,40 @@ Ejercicio 5: Comprobación de Ordenación
 Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
-*//*
-int checkSorted(int arr[], int size){ 
+*/
+int checkSorted(int arr[], int size)
+{
+int AscOrDes;
 
-    } 
-  
-       
+    for (int i = 0; i < size; i++)
+      {
+        if (arr[i]<arr[i+1]
+        {
+          AscOrDes = 1;
+        }
+        if (arr[i]>arr[i+1])
+        {
+          AscOrDes = -1;
+        }
+        else
+        {
+          AscOrDes = 0;
+        }
+      }
 
+  if (AscOrDes == 1)
+  {
+    return 1;
+  }
+  if (AscOrDes == 0)
+  {
+    return 0;
+  }
+  if (AscOrDes = -1)
+  {
+    return -1;
+  }
+}
   
   
   
@@ -113,10 +140,8 @@ int checkSorted(int arr[], int size){
   
   
   
-  
-  return -2; }
 
-*//*
+/*
 Ejercicio 6: Información de una Biblioteca
 Descripción: Vamos a representar la información de una biblioteca. En la
 biblioteca, hay libros, y cada libro tiene un autor. Queremos organizar esta
